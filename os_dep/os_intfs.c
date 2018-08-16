a53b3a53390dac06c0d822a9a95bae5d298e0d05
Andrey Panov <rockford@yandex.ru>
2018-08-16 15:35:44 +0300

    rtl8812au: Make it compile with 4.19

--- os_dep/os_intfs.c
+++ os_dep/os_intfs.c
@@ -972,7 +972,7 @@ static unsigned int rtw_classify8021d(struct sk_buff *skb)
 	return dscp >> 5;
 }
 
-static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb, void *accel_priv, select_queue_fallback_t fallback)
+static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb, struct net_device  *accel_priv, select_queue_fallback_t fallback)
 {
 	_adapter	*padapter = rtw_netdev_priv(dev);
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
